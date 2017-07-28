5e3b29473caa50cabf2ad677ece35469d80ece00
bdynamite <bdynamite@yandex.ru>
2017-07-28 17:23:16 +0300

    commit#1

--- README.md
+++ README.md
@@ -12,6 +12,28 @@ pip install -r requirements.txt # alternatively try pip3
 
 Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
 
+# Quickstart
+
+
+Example of script launch on Linux, Python 3.5:
+
+```#!bash
+
+$ python vk_friends_online.py
+
+login: ivan@gmail.com
+password: *****
+
+# output example
+online friends:
+Виктор Сухоруков
+Илья Панов
+...
+...
+
+
+```
+
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,16 @@
 import vk
+import getpass
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    return input('login: ')
 
 
 def get_user_password():
-    pass
+    return getpass.getpass('password: ')
 
 
 def get_online_friends(login, password):
@@ -17,13 +18,17 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope = 'friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    friends_id = api.friends.getOnline(online_mobile1=1)
+    friends_info = api.users.get(user_ids=friends_id)
+    return friends_info
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('online friends: ')
+    print('\n'.join([f'{x["first_name"]} {x["last_name"]}' for x in friends_online]))
 
 if __name__ == '__main__':
     login = get_user_login()
